7f4651ece11d9d0294419f718ced828a1f7817d4
Administrator <3294697230@qq.com>
2024-03-13 09:42:52 +0800

    Do not download this branch.

--- src/main/java/com/github/winhan/diao/mixin/ExampleMixin.java
+++ src/main/java/com/github/winhan/diao/mixin/ExampleMixin.java
@@ -1,6 +1,10 @@
 package com.github.winhan.diao.mixin;
 
+import com.mojang.logging.LogUtils;
+import net.fabricmc.loader.impl.util.log.Log;
+import net.fabricmc.loader.impl.util.log.LogCategory;
 import net.minecraft.server.MinecraftServer;
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,6 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "loadWorld")
 	private void init(CallbackInfo info) {
+		Log.info(LogCategory.GAME_PROVIDER, "NOnoNonO");
 		// This code is injected into the start of MinecraftServer.loadWorld()V
 	}
 }
