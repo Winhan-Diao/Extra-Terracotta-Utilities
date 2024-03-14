917850a7d681d406a5f5b108dad2d6c67d915851
Administrator <3294697230@qq.com>
2024-03-14 14:57:08 +0800

    Do not download this branch.

---
+++ src/main/java/com/github/winhan/diao/AllayedEnderizedMagentaGlazedTerracotta.java
@@ -0,0 +1,9 @@
+package com.github.winhan.diao;
+
+import net.minecraft.block.Block;
+
+public class AllayedEnderizedMagentaGlazedTerracotta extends EnderizedMagentaGlazedTerracotta {
+    public AllayedEnderizedMagentaGlazedTerracotta(Settings settings) {
+        super(settings);
+    }
+}
